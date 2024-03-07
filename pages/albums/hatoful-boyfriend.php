754583049cb1763d2be8e3039405fdfe7a85ec89
Quails <centimofu@gmail.com>
2024-03-07 17:44:50 +0000

    :necktie: Display collected cards in deck
    Adds a new property to the deck class to record collected card numbers and checks for these numbers in the render function.

--- pages/albums/hatoful-boyfriend.php
+++ pages/albums/hatoful-boyfriend.php
@@ -15,11 +15,13 @@
     protected string $label;
     protected string $slug;
     protected string $colour;
+    protected array $collected;
 
-    public function __construct(string $label, string $colour) {
+    public function __construct(string $label, string $colour, array $collected = []) {
       $this->label = $label;
       $this->slug = strtolower(str_replace(" ", "", $label));
       $this->colour = $colour;
+      $this->collected = $collected;
     }
 
     public function renderDeck() {
@@ -27,11 +29,13 @@
       echo "<h2 class=\"deck__header deck__header--" . $this->colour . "\">" . $this->label . "</h2>";
       echo "<table>";
       for ($i = 1; $i <= 20; $i++) {
+        $cardNumber = in_array($i, $this->collected) ? sprintf('%02d', $i) : '00';
+
         if ($i % 5 === 1) {
           echo "<tr>";
         }
         echo "<td>";
-        echo "<img src=\"..\..\img\cards\\" . $this->slug . "00.gif\" />";
+        echo "<img src=\"..\..\img\cards\\" . $this->slug . $cardNumber . ".gif\" />";
         echo "</td>";
         if($i % 5 === 0 ) {
           echo "</tr>";
@@ -46,12 +50,12 @@
     new Deck("Mourning", Colour::Green),
     new Deck("Judecca", Colour::Blue),
     new Deck("Cleaver", Colour::Purple),
-    new Deck("Coolene", Colour::Blue),
+    new Deck("Coolene", Colour::Blue, [1, 16]),
     new Deck("Hawk Party", Colour::Blue),
     new Deck("Narcoleptic", Colour::Yellow),
     new Deck("Artistic", Colour::Orange),
     new Deck("Everybirdie", Colour::Special),
-    new Deck("Coo", Colour::Brown),
+    new Deck("Coo", Colour::Brown, [6]),
     new Deck("Playbirdie", Colour::Grey),
     new Deck("Fantail", Colour::Blue),
     new Deck("Red Scarf", Colour::Red),
