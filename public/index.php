4d9b0085c12de770582cb5a9c81427f6f00f11b1
Quails <centimofu@gmail.com>
2024-03-12 22:01:48 +0000

    :recycle: Refactor router initialization
    Moves the route declarations into a separate file and adds a static loader to the router class.

--- public/index.php
+++ public/index.php
@@ -1,13 +1,6 @@
 <?php
 
 require __DIR__ . '/../src/utilities/router.php';
+require __DIR__ . '/../src/utilities/request.php';
 
-$router = new Router;
-
-$router->register('', __DIR__ . '/../src/controllers/index.controller.php');
-$router->register('albums', __DIR__ . '/../src/controllers/albums.controller.php');
-$router->register('trading', __DIR__ . '/../src/controllers/trading.controller.php');
-$router->register('mastered', __DIR__ . '/../src/controllers/mastered.controller.php');
-$router->register('logs', __DIR__ . '/../src/controllers/logs.controller.php');
-
-require $router->direct(trim($_SERVER['REQUEST_URI'], '/'));
+require Router::load(__DIR__ . '/../src/utilities/routes.php')->direct(Request::uri());
--- src/utilities/router.php
+++ src/utilities/router.php
@@ -3,6 +3,12 @@
 class Router {
     protected $routes = [];
 
+    public static function load($file) {
+        $router = new static;
+        require $file;
+        return $router;
+    }
+
     public function register(string $uri, string $controllerPath) {
         $this->routes[$uri] = $controllerPath;
     }
