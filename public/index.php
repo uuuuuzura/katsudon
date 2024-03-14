9f8a41c39a483864c8d8cdb8036538e0fee18a34
Quails <centimofu@gmail.com>
2024-03-14 20:18:11 +0000

    :card_file_box: Connect to database
    Creates a class to connect to and query the database

--- public/index.php
+++ public/index.php
@@ -1,6 +1,16 @@
 <?php
 
+// TODO: Remove debugging before pushing to production
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 require __DIR__ . '/../src/utilities/router.php';
 require __DIR__ . '/../src/utilities/request.php';
+require __DIR__ . '/../src/utilities/database.php';
+
+$config = require('../config/credentials.php');
+
+$db = new Database($config['connectionString'], $config['username'], $config['password']);
 
 require Router::load(__DIR__ . '/../src/utilities/routes.php')->direct(Request::uri());
---
+++ src/utilities/database.php
@@ -0,0 +1,19 @@
+<?php
+
+class Database {
+    public $connection;
+
+    public function __construct ($config, $username, $password) {
+        $dsn = "mysql:" . http_build_query($config, '', ';');
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query (string $sql) {
+        $statement = $this->connection->prepare($sql);
+        $statement->execute();
+
+        return $statement;
+    }
+}
