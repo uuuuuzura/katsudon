9c1053f0fd3bd6c0d12534f8c4cd6d105d5498a9
Quails <centimofu@gmail.com>
2024-03-15 21:26:37 +0000

    :fire: Remove old album pages
    Removes the static album pages from the public folder.

--- public/pages/albums/akiba-maid-war.php
+++
@@ -1,6 +0,0 @@
-<main>
-  <h1 class="pagename">Akiba Maid War</h1>
-  <p>
-    Under Construction!
-  </p>
-</main>
--- public/pages/albums/nu-carnival.php
+++
@@ -1,6 +0,0 @@
-<main>
-  <h1 class="pagename">Nu:Carnival</h1>
-  <p>
-    Under Construction!
-  </p>
-</main>
