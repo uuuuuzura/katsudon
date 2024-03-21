8b243f6275780b90b70f0f0115237ea7c0ac9e78
Quails <centimofu@gmail.com>
2024-03-21 18:45:08 +0000

    :sparkles: Display trading cards on trading page

--- src/controllers/trading.controller.php
+++ src/controllers/trading.controller.php
@@ -1,3 +1,5 @@
 <?php
 
+$tradingCards = $db->query("SELECT slug FROM cards WHERE status = 'trading' ORDER BY slug ASC")->fetchAll();
+
 require __DIR__ . '/../views/trading.view.php';
--- src/views/trading.view.php
+++ src/views/trading.view.php
@@ -2,9 +2,25 @@
 
 <main>
   <h1 class="pagename">Trading</h1>
-  <p>
-    Under Construction!
-  </p>
+  <section class="section">
+    <h2 class="section__header section__header--step-1">Guidelines</h2>
+    <ul>
+      <li>Please conduct all trades under <a href="#">this Dreamwidth entry</a>
+      <li>Only cards in this section are eligable for trade</li>
+      <li>There is no limit on the amount traded at one time</li>
+    </ul>
+  </section>
+  <section class="section">
+    <h2 class="section__header section__header--step-2">Cards</h2>
+    <div class="section__container">
+      <?php if (sizeof($tradingCards) === 0) {
+        echo "<p>Currently Empty!</p>";
+      } else {
+        foreach ($tradingCards as $card) {
+          echo "<img alt=\"" . $card['slug'] . "\" src=\"..\..\img\cards\\" . $card['slug'] . ".gif\" />";
+        }
+      } ?>
+    </div>
   </section>
 </main>
 
