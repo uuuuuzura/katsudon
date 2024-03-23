6361e7f939057bc8e70133a612d141f569f31d8a
Quails <centimofu@gmail.com>
2024-03-23 10:02:45 +0000

    :recycle: Refactor level banner logic
    Moves the level banner logic inside of the level class.

--- src/controllers/index.controller.php
+++ src/controllers/index.controller.php
@@ -2,20 +2,7 @@
 
 require (__DIR__ . '/../models/level.model.php');
 
-function roundMaxCardNumber($cardCount) {
-    if($cardCount <= 800) {
-        return ceil($cardCount / 100) * 100;
-    }
-
-    if($cardCount <= 2400) {
-        return ceil($cardCount / 200) * 200;
-    }
-
-    return ceil($cardCount / 300) * 300;
-}
-
 $cardCount = $db->query("SELECT COUNT(*) FROM cards")->fetchColumn();
-$levelData = $db->query("SELECT label, bannerImg FROM levels WHERE maxCardCount = :cardCount", ['cardCount' => roundMaxCardNumber($cardCount)])->fetch();
-$level = new Level($levelData['label'], $levelData['bannerImg']);
+$level = new Level($cardCount, $db);
 
 require __DIR__ . '/../views/index.view.php';
--- src/models/level.model.php
+++ src/models/level.model.php
@@ -5,10 +5,24 @@ class Level
     protected string $label;
     protected string $bannerImg;
 
-    public function __construct(string $label, string $bannerImg)
+    public function __construct(int $cardCount, $db)
     {
-        $this->label = $label;
-        $this->bannerImg = $bannerImg;
+        $level = $db->query("SELECT label, bannerImg FROM levels WHERE maxCardCount = :cardCount", ['cardCount' => $this->roundMaxCardNumber($cardCount)])->fetch();
+        $this->label = $level['label'];
+        $this->bannerImg = $level['bannerImg'];
+    }
+
+    private function roundMaxCardNumber($cardCount)
+    {
+        if ($cardCount <= 800) {
+            return ceil($cardCount / 100) * 100;
+        }
+
+        if ($cardCount <= 2400) {
+            return ceil($cardCount / 200) * 200;
+        }
+
+        return ceil($cardCount / 300) * 300;
     }
 
     public function renderBanner()
