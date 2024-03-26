1768a88f9772830d5211bb07df293e039020a111
Quails <centimofu@gmail.com>
2024-03-26 17:10:33 +0000

    :speech_balloon: Add Colors TCG link to footer

--- src/views/includes/footer.include.php
+++ src/views/includes/footer.include.php
@@ -1,5 +1,8 @@
       <footer class="footer">
         <p class="footer__paragraph">
+          Trading card collection for <a class="link" target="_blank" href="https://colors-tcg.eu/index.php">Colors TCG</a>
+        <p>
+        <p class="footer__paragraph">
           Banner image by <a class="link" target="_blank" href="https://twitter.com/okazakiokaa">おかざきおか</a> 🎴
           Background from <a class="link" target="_blank" href="https://www.toptal.com/designers/subtlepatterns/">Subtle Patterns</a> 🎴
           Part of <a class="link" href="https://quailmail.moe/">quailmail.moe</a>
